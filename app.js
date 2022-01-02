20cce2604c052902a2c107a0f2ae7670be05fa28
Aapo Laakkio <aapolaakkio@outlook.com>
2022-01-02 14:09:02 +0200

    Initialize project

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const serverPort = 8080;
+const app = express();
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use(express.static('public'));
