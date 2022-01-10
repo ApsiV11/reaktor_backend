26aae907d81d33925322caadb9f7cadfb629b224
Aapo Laakkio <aapolaakkio@outlook.com>
2022-01-10 19:49:59 +0200

    -

--- utils/populator.js
+++ utils/populator.js
@@ -38,7 +38,7 @@ const downloadDatabase = async () => {
         if(isCursorFound.length>0 && thisPage===isCursorFound[0].cursor) {
             cursor = null;
         }
-        else if(cursor && cursor!="/rps/history"){
+        else if(cursor && cursor!=="/rps/history"){
             await Cursor.create({"cursor": cursor});
         }
 
